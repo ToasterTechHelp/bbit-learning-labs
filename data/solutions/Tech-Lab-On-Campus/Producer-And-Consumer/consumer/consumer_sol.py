126e5ed09dc750975f9ce52ebf3acdf1aa69d2ad
Zakaria Khan <159575539+zakariaxk@users.noreply.github.com>
2026-02-10 23:06:39 +0000

    implemented mqConsumer class

--- data/solutions/Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_sol.py
+++ data/solutions/Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_sol.py
@@ -18,7 +18,7 @@ import pika
 from consumer_interface import mqConsumerInterface
 
 
-class mqConsumer(mqConsumerInterface):
+class mqConsumer():
     def __init__(
         self, binding_key: str, exchange_name: str, queue_name: str
     ) -> None:
--- tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -1,27 +1,15 @@
-# Copyright 2024 Bloomberg Finance L.P.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import os
 import pika
+from consumer_interface import mqConsumerInterface
 
-class mqConsumerInterface:
+class mqConsumer():
     def __init__(
-        self, binding_key: str, exchange_name: str, queue_name: str
-    ) -> None:
+        self, binding_key: str, exchange_name: str, queue_name: str) -> None:
         # Save parameters to class variables
         self.binding_key = binding_key
         self.exchange_name = exchange_name
         self.queue_name = queue_name
+
         # Call setupRMQConnection
         self.setupRMQConnection()
 
@@ -41,10 +29,10 @@ class mqConsumerInterface:
         self.channel.exchange_declare(exchange=self.exchange_name)
 
         # Bind Binding Key to Queue on the exchange
-        channel.queue_bind(
+        self.channel.queue_bind(
             queue= self.queue_name,
             routing_key= self.binding_key,
-            exchange= self.exchange.name,
+            exchange= self.exchange_name,
         )
 
         # Set-up Callback function for receiving messages
@@ -56,10 +44,10 @@ class mqConsumerInterface:
         self, channel, method_frame, header_frame, body
     ) -> None:
         # Acknowledge message
-        channel.basic_ack(method_frame.delivery_tag, False)
+        self.channel.basic_ack(method_frame.delivery_tag, False)
 
         #Print message (The message is contained in the body parameter variable)
-        print("Message received: ", body)
+        print("Received message: ", body)
 
 
     def startConsuming(self) -> None:
